2dbbce2f2f10724424d2395d8a34ed0e2bd776f8
Kashif Nazir <kashifsnazir@gmail.com>
2023-04-29 20:13:09 -0700

    Logging

---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,21 @@
+#include "Log.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Hazel
+{
+
+/*static*/ std::shared_ptr<spdlog::logger> Log::sCoreLogger;
+/*static*/ std::shared_ptr<spdlog::logger> Log::sClientLogger;
+
+void Log::Init()
+{
+	spdlog::set_pattern("%^[%T] %n: %v%$");
+
+	sCoreLogger = spdlog::stdout_color_mt("HAZEL");
+	sCoreLogger->set_level(spdlog::level::trace);
+
+	sClientLogger = spdlog::stdout_color_mt("APP");
+	sClientLogger->set_level(spdlog::level::trace);
+}
+}
