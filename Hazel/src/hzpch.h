84e640376a38f5a4cbae211cfd72e0e486c71ff3
Kashif Nazir <kashifsnazir@gmail.com>
2023-04-30 01:52:11 -0700

    Precompiled Headers

---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <utility>
+
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
